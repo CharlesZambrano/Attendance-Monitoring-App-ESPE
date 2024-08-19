4f2b8c064196b86743d56c1380bfa4c0f321aa63
CharlesZambrano <cdzambrano3@espe.edu.ec>
2024-08-19 11:12:42 -0500

    [FIX] Scss modals

--- src/components/ClassCard/ClassCard.jsx
+++ src/components/ClassCard/ClassCard.jsx
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import placeholderIcon from '../../assets/placeholder.png';
-import speechIcon from '../../assets/speech.png'; // Import tardiness icon
-import API_ENDPOINTS from '../../routes/apiEndpoints';
-import './ClassCard.scss';
+import React, { useEffect, useState } from "react";
+import placeholderIcon from "../../assets/placeholder.png";
+import speechIcon from "../../assets/speech.png"; // Import tardiness icon
+import API_ENDPOINTS from "../../routes/apiEndpoints";
+import "./ClassCard.scss";
 
 const ClassCard = ({ schedule, onCardClick }) => {
-  const { CLASS_SCHEDULE_ID, SUBJECT, NRC, TYPE, START_TIME, END_TIME } = schedule;
+  const { CLASS_SCHEDULE_ID, SUBJECT, NRC, TYPE, START_TIME, END_TIME } =
+    schedule;
   const [attendanceInfo, setAttendanceInfo] = useState(null);
 
   useEffect(() => {
     const fetchAttendanceInfo = async () => {
       try {
-        const response = await fetch(`${API_ENDPOINTS.GET_CLASS_SCHEDULE_ATTENDANCE}/${CLASS_SCHEDULE_ID}`);
+        const response = await fetch(
+          `${API_ENDPOINTS.GET_CLASS_SCHEDULE_ATTENDANCE}/${CLASS_SCHEDULE_ID}`
+        );
         const data = await response.json();
 
         if (response.ok && data.length > 0) {
           setAttendanceInfo(data[0]); // Assume only one attendance record per class
         } else if (!response.ok) {
-          console.error("Error fetching attendance:", data.message || data.error);
+          console.error(
+            "Error fetching attendance:",
+            data.message || data.error
+          );
         } else {
-          console.warn(`No attendance records found for CLASS_SCHEDULE_ID ${CLASS_SCHEDULE_ID}`);
+          console.warn(
+            `No attendance records found for CLASS_SCHEDULE_ID ${CLASS_SCHEDULE_ID}`
+          );
         }
       } catch (error) {
         console.error("Error fetching attendance:", error);
@@ -31,29 +39,29 @@ const ClassCard = ({ schedule, onCardClick }) => {
 
   // Function to manually adjust and format the time
   const formatFullDateTime = (time) => {
-    if (!time) return '';  // Handle null times
+    if (!time) return ""; // Handle null times
 
     const date = new Date(time);
     date.setHours(date.getHours() - 7); // Manually subtract 5 hours to adjust for Ecuador timezone
 
-    return date.toLocaleTimeString('es-ES', {
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
+    return date.toLocaleTimeString("es-ES", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
     });
   };
 
   // Function to format "Record Start Time" and "Record End Time" that comes only with time
   const formatTimeOnly = (time) => {
-    if (!time) return '';  // Handle null times
+    if (!time) return ""; // Handle null times
 
-    const [hours, minutes, seconds] = time.split(':');
+    const [hours, minutes, seconds] = time.split(":");
     const date = new Date();
     date.setHours(hours, minutes, seconds);
-    return date.toLocaleTimeString('es-ES', {
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
+    return date.toLocaleTimeString("es-ES", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
     });
   };
 
@@ -64,14 +72,25 @@ const ClassCard = ({ schedule, onCardClick }) => {
       </div>
       <div className="card-content">
         <h3>{SUBJECT}</h3>
-        <p><strong>NRC:</strong> {NRC}</p>
-        <p><strong>Tipo:</strong> {TYPE}</p>
+        <p>
+          <strong>NRC:</strong> {parseInt(NRC, 10)}
+        </p>{" "}
+        {/* Convert NRC to an integer */}
+        <p>
+          <strong>Tipo:</strong> {TYPE}
+        </p>
         <p>
           <strong>Hora de inicio:</strong> {formatFullDateTime(START_TIME)}
           {attendanceInfo?.ENTRY_TIME && (
             <span className="aligned-right">
-              <strong>Hora de registro Inicio:</strong> {formatTimeOnly(attendanceInfo.ENTRY_TIME)}{' '}
-              {attendanceInfo.LATE_ENTRY === 'SI' && <span className="late-text">(Tarde <img src={speechIcon} alt="Tarde" className="late-icon" />)</span>}
+              <strong>Hora de registro Inicio:</strong>{" "}
+              {formatTimeOnly(attendanceInfo.ENTRY_TIME)}{" "}
+              {attendanceInfo.LATE_ENTRY === "SI" && (
+                <span className="late-text">
+                  (Tarde{" "}
+                  <img src={speechIcon} alt="Tarde" className="late-icon" />)
+                </span>
+              )}
             </span>
           )}
         </p>
@@ -79,8 +98,14 @@ const ClassCard = ({ schedule, onCardClick }) => {
           <strong>Hora de fin:</strong> {formatFullDateTime(END_TIME)}
           {attendanceInfo?.EXIT_TIME && (
             <span className="aligned-right">
-              <strong>Hora de registro Fin:</strong> {formatTimeOnly(attendanceInfo.EXIT_TIME)}{' '}
-              {attendanceInfo.LATE_EXIT === 'SI' && <span className="late-text">(Tarde <img src={speechIcon} alt="Tarde" className="late-icon" />)</span>}
+              <strong>Hora de registro Fin:</strong>{" "}
+              {formatTimeOnly(attendanceInfo.EXIT_TIME)}{" "}
+              {attendanceInfo.LATE_EXIT === "SI" && (
+                <span className="late-text">
+                  (Tarde{" "}
+                  <img src={speechIcon} alt="Tarde" className="late-icon" />)
+                </span>
+              )}
             </span>
           )}
         </p>
